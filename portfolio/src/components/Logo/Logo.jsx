ab53b95935d0aa216692c1230eed6e9d7c20889f
shawn-chambers <chambers.shawn.89@gmail.com>
2020-05-27 19:32:17 -0700

    React set-up and proof of concept

---
+++ portfolio/src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import icon from '../../assets/shawnipedia.png';
+
+const Logo = () => {
+  return <img src={icon} className='logo' alt='Icon' style={{maxHeight: '75%', maxWidth: 'auto'}}/>
+}
+
+export default Logo;
---
+++ portfolio/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './components/Top/Main';
+
+const App = () => {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
