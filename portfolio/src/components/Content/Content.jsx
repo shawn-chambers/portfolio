388f629f62fa5509a47cdf7ccdfe7abf83f73a41
shawn-chambers <chambers.shawn.89@gmail.com>
2021-04-27 15:14:05 -0700

    added ability to position content box with fetched json

--- portfolio/src/components/Content/Content.jsx
+++ portfolio/src/components/Content/Content.jsx
@@ -5,20 +5,33 @@ import Picture from "../Pictures/Picture";
 import Header from "../Header/Header";
 
 const Content = ({ content }) => {
+  let displayed = false;
   const handleLinks = (string) => {
-    let content = string.split("[").map((el) => el.split("]"));
-    let [linkWord, url] = content[1][0].split(" ");
+    let content = string
+      .split("{")
+      .map((el) => el.split("}"))
+      .flat();
+
+    let text = content.map((el) =>
+      el.substring(0, 7) === '"link":' ? JSON.parse(`{${el}}`) : el
+    );
 
     return (
-      <>
-        <span>{content[0][0]}</span>
-        <span>
-          <a href={url} target="_blank" rel="noreferrer noopener">
-            {linkWord}
-          </a>
-        </span>
-        <span>{content[1][1]}</span>
-      </>
+      <p>
+        {text.map((el, i) => {
+          if (typeof el === "object") {
+            return (
+              <span key={`content-link-${i}`}>
+                <a href={el.url} target="_blank" rel="noreferrer noopener">
+                  {el.link}
+                </a>
+              </span>
+            );
+          } else {
+            return <span key={`content-${i}`}>{el}</span>;
+          }
+        })}
+      </p>
     );
   };
 
@@ -37,24 +50,46 @@ const Content = ({ content }) => {
             {content.pictures && <Picture pictures={content.pictures} />}
             {content.description.map((paragraph, i) => {
               if (typeof paragraph === "object") {
+                if (paragraph.box) {
+                  displayed = true;
+                  return (
+                    <ContentsBox contents={content.box} />
+                  )
+                }
                 return (
                   <>
-                    {console.log("paragraph", paragraph)}
-                    {paragraph.header ? (
-                      <h3 className={paragraph.header}>{paragraph.header}</h3>
+                    {paragraph.section ? (
+                      <>
+                        <div className="resume__content--section-header">
+                          <h2 key={`header-${i}`}>{paragraph.section}</h2>
+                          <hr></hr>
+                        </div>
+                      </>
                     ) : null}
-                    <p key={`paragraph-${i}`}>
-                      {paragraph.link
-                        ? handleLinks(paragraph.content)
-                        : paragraph.content}
-                    </p>
+                    {paragraph.header ? (
+                      <>
+                        <h3 key={`header-${i}`} className={paragraph.header}>
+                          {paragraph.header}
+                        </h3>
+                      </>
+                    ) : null}
+                    <>
+                      <p
+                        key={`paragraph-${i}`}
+                        className={paragraph.indent ? "indent" : ""}
+                      >
+                        {paragraph.link
+                          ? handleLinks(paragraph.content)
+                          : paragraph.content}
+                      </p>
+                    </>
                   </>
                 );
               } else {
                 return <p key={`paragraph-${i}`}>{paragraph}</p>;
               }
             })}
-            <ContentsBox contents={content.box} />
+            {!displayed && <ContentsBox contents={content.box} />}
           </div>
         </div>
         <Footer />
@@ -64,7 +99,3 @@ const Content = ({ content }) => {
 };
 
 export default Content;
-
-/*
-https://en.wikipedia.org/w/skins/Vector/resources/skins.vector.styles/images/tab-current-fade.png?22887
-*/
